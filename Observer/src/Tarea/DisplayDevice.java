8edd57fa8f48f26c94a167bd957ccfeb02268b1a
JoeyBustamante <jopabust@espol.edu.ec>
2025-06-10 20:46:29 -0500

    ahora si

--- Observer/src/Tarea/DisplayDevice.java
+++
@@ -1,8 +0,0 @@
-package Tarea;
-
-public class DisplayDevice {
-    public void update(int newTemperature) {
-        System.out.println("Display: nueva temperatura → " + newTemperature + "°C");
-    }
-}
-
